7949ce5c0e2b4f004d3249d327af7ffad9eeb023
Sam Knowles <samuelkcknowles@gmail.com>
2018-11-04 21:19:35 -0700

    added grid template rows

---
+++ src/Compontents/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './Home';
+import '../Styles/Home.css'
+
+function App() {
+    return (
+        <div>
+            <Home></Home>
+        </div>
+    )
+}
+
+export default App;
---
+++ src/Compontents/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className="grid-container2">
+            <div className="item1">Header</div>
+            <div className="item2">Menu</div>
+            <div className="item3">Main</div>
+            <div className="item4">Right</div>
+            <div className="item5">Footer</div>
+        </div>
+    )
+}
+
+export default Home
